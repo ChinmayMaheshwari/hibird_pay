3a66fa52a68355d2bcdeacc9a16250c6b5ba81fe
Chinmay Maheshwari <chinmay12456789@gmail.com>
2020-09-07 19:25:03 +0530

    reset password api

--- wipay/settings.py
+++ wipay/settings.py
@@ -54,7 +54,7 @@ REST_FRAMEWORK = {
       ],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
+        #'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '3/hour',
