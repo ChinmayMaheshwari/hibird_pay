05d949774940fcaa4b63c03a9c716a3de9a954b6
Chinmay Maheshwari <chinmay12456789@gmail.com>
2020-09-08 10:06:27 +0530

    About to Finsih

--- userpay/serializers.py
+++ userpay/serializers.py
@@ -3,15 +3,16 @@ from .models import *
 from datetime import datetime
 
 class TransactionDetailSerializer(serializers.ModelSerializer):
-    # invoice = serializers.SerializerMethodField('slug_field')
-    # def slug_field(self,obj):
-    #     return obj.invoice_file
+   # invoice = serializers.CharField(source='invoice_file')
+    invoice = serializers.SerializerMethodField('slug_field')
+    def slug_field(self,obj):
+        return 'http://hybird.herokuapp.com/media/'+str(obj.invoice_file)
     date = serializers.SerializerMethodField('format_date')
     def format_date(self,obj):
     	return obj.date.strftime('%I:%M %p  %d-%b-%Y')
     class Meta:
         model = TransactionDetail
-        fields = ['order_id','payment_id','date','success','amount','invoice_file',]
+        fields = ['order_id','payment_id','date','success','amount','invoice',]
 
 class PlanSerializer(serializers.ModelSerializer):
 	class Meta:
